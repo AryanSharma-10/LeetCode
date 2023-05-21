db209c0323f34c206ebbe1e2d73660d5cc06f317
Aryan Sharma <120492309+AryanSharma-10@users.noreply.github.com>
2023-05-22 03:10:45 +0530

    Time: 1151 ms (10.14%), Space: 51.5 MB (6.42%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        bool route=false;
+        vector<int>dp(nums.size(),0);
+
+        return path(nums,0,route,dp);
+    }
+
+    bool path(vector<int>& nums, int index, bool route, vector<int>&dp)
+    {   
+        if(index==nums.size()-1)
+            return true;
+        
+
+        for(int i=1;i<=nums[index];i++)
+        {   
+            if(index+i<nums.size() && dp[index+i]!=1)
+                route=path(nums, i+index, route,dp);
+            
+            if(route)
+                return true;
+        }
+
+        dp[index]=1;
+        
+        return route;
+    }
+
+           
+};
