aa35562e3f6a9d95f56979d1ffe9ff4c8b15ac0a
Aryan Sharma <120492309+AryanSharma-10@users.noreply.github.com>
2023-09-02 13:49:51 +0530

    Time: 31 ms (11.82%), Space: 44 MB (42.05%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.java
@@ -18,7 +18,6 @@ class Solution {
         while(curr.next!=null)
         {
             int n=gcd(curr.val, curr.next.val);
-            System.out.println(n);
             ListNode temp=new ListNode(n, curr.next);
             curr.next=temp;
             curr=temp.next;
@@ -29,7 +28,7 @@ class Solution {
 
     public static int gcd(int n1, int n2)
     {
-         int gcd = 1;
+        int gcd = 1;
 
         for (int i = 1; i <= n1 && i <= n2; ++i) {
             if (n1 % i == 0 && n2 % i == 0)
