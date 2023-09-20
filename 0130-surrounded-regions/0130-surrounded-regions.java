d44c4e52b989b55805888ba50fb1d858aea88145
Aryan Sharma <120492309+AryanSharma-10@users.noreply.github.com>
2023-09-20 13:31:03 +0530

    Time: 2 ms (75.62%), Space: 43.9 MB (55.17%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,94 @@
+class Solution {
+    public static void dfs(char[][] board, int [][] vis, int row, int col, int m, int n)
+    {
+        if(row > 0 && board[row - 1][col] == 'O' && vis[row - 1][col] == 0)
+        {
+            vis[row - 1][col] = 1;
+            dfs(board, vis, row - 1, col, m, n);
+        }
+
+        if(col + 1 < n && board[row][col + 1] == 'O' && vis[row][col+1] == 0)
+        {
+            vis[row][col+1] = 1;
+            dfs(board, vis, row, col + 1, m, n);
+        }
+
+        if(row + 1 < m && board[row + 1][col] == 'O' && vis[row + 1][col] == 0)
+        {
+            vis[row + 1][col] = 1;
+            dfs(board, vis, row + 1, col, m, n);
+        }
+
+        if(col > 0 && board[row][col - 1] == 'O' && vis[row][col - 1] == 0)
+        {
+            vis[row][col - 1] = 1;
+            dfs(board, vis, row, col - 1, m, n);
+        }
+    }
+    
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        int [][] visited = new int[m][n];
+
+        int i = -1;
+        int j = 0;
+
+        while(++i < m)
+        {
+            if(visited[i][j] == 0 && board[i][j] == 'O')
+            {
+                visited[i][j] = 1;
+                dfs(board, visited, i, j, m, n);
+            }
+        }
+
+        i = 0;
+        j = -1;
+
+        while(++j < n)
+        {
+            if(visited[i][j] == 0 && board[i][j] == 'O')
+            {
+                visited[i][j] = 1;
+                dfs(board, visited, i, j, m, n);
+            }
+        }
+
+        i = -1;
+        j = n - 1;
+
+        while(++i < m)
+        {
+            if(visited[i][j] == 0 && board[i][j] == 'O')
+            {
+                visited[i][j] = 1;
+                dfs(board, visited, i, j, m, n);
+            }
+        }
+
+        i = m - 1;
+        j = -1;
+
+        while(++j < n)
+        {
+            if(visited[i][j] == 0 && board[i][j] == 'O')
+            {
+                visited[i][j] = 1;
+                dfs(board, visited, i, j, m, n);
+            }
+        }
+
+        for(i = 0 ; i < m ; i++)
+        {
+            for(j = 0 ; j <n ; j++)
+            {
+                if(visited[i][j] == 0 && board[i][j] == 'O')
+                    board[i][j] = 'X';
+                
+            }
+        }
+
+    }
+}
